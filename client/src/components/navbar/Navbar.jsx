21bc38bc6d9e6d7dc4b03f2bf9a1f4eb88980a68
TranDinhHieu <hieu.tran11002@gmail.com>
2026-06-13 08:30:48 +0700

    Build responsive header

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import "./navbar.scss"
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>Homia</span>
+        </a>
+        <a href="/">Nhà Đẹp</a>
+        <a href="/">Đất Tốt</a>
+        <a href="/">Chúng Tôi</a>
+        <a href="/">Hỗ Trợ</a>
+      </div>
+      <div className="right">
+        <a href="/">Đăng Nhập</a>
+        <a href="/" className="register">Đăng Ký</a>
+
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={() => setOpenMenu(!openMenu)} />
+        </div>
+        <div className={openMenu ? "menu active" : "menu"}>
+          <button className="closeBtn" onClick={() => setOpenMenu(false)}>✕</button>
+          <a href="/">Nhà Đẹp</a>
+          <a href="/">Đất Tốt</a>
+          <a href="/">Chúng Tôi</a>
+          <a href="/">Hỗ Trợ</a>
+          <a href="/">Đăng Nhập</a>
+          <a href="/">Đăng Ký</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
