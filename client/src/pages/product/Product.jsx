6b88a71e4354635f3fb90e6a673a5b8a233e92a5
TranDinhHieu <hieu.tran11002@gmail.com>
2026-06-15 16:11:30 +0700

    Update product details page - add descriptive information

--- client/src/pages/product/Product.jsx
+++ client/src/pages/product/Product.jsx
@@ -2,6 +2,7 @@ import Slider from "../../components/shared/slider/Slider"
 import "./product.scss"
 import { singlePost, userData } from "../../libs/dummyData"
 import Price from "../../components/shared/price/Price"
+import Map from "../../components/shared/map/Map"
 
 const Product = () => {
   return (
@@ -17,7 +18,7 @@ const Product = () => {
                   <img src="/pin.png" alt="" />
                   <span>{singlePost.address}</span>
                 </div>
-                <Price price={singlePost.price} sm />
+                <Price price={singlePost.price} />
               </div>
               <div className="user">
                 <img src={userData.img} alt="" referrerPolicy="no-referrer" />
@@ -31,7 +32,85 @@ const Product = () => {
         </div>
       </div>
       <div className="features">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <p className="title">Thông tin chung</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Tiện ích</span>
+                <p>Người thuê tự chi trả</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Thú cưng</span>
+                <p>Cho phép</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Khoản Phí</span>
+                <p>Tổng thu nhập của hộ gia đình phải gấp ít nhất 3 lần tiền thuê nhà.</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Kích cỡ tài sản</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>80m2</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>2 phòng ngủ</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="" />
+              <span>1 phòng tắm</span>
+            </div>
+          </div>
+          <p className="title">Những địa điểm gần đó</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>Trường học</span>
+                <p> Cách 250m</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Trạm xe buýt</span>
+                <p> Cách 150m</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Nhà hàng</span>
+                <p> Cách 200m</p>
+              </div>
+            </div>
+          </div>
+          <p className="title">Vị trí</p>
+          <div className="mapContainer">
+            <Map list={[singlePost]} />
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Gửi tin nhắn
+            </button>
+            <button>
+              <img src="/save.png" alt="" />
+              Lưu vị trí
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
