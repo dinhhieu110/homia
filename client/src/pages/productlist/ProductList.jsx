6eb5f7d1ce2037d0695d87e51ee22b8c26a8bb74
TranDinhHieu <hieu.tran11002@gmail.com>
2026-06-15 09:08:44 +0700

    Build product list page

--- client/src/pages/productlist/ProductList.jsx
+++ client/src/pages/productlist/ProductList.jsx
@@ -2,6 +2,7 @@ import "./productlist.scss"
 import Filter from "../../components/shared/filter/Filter"
 import Card from "../../components/shared/card/Card"
 import { dummyData } from "./dummyData"
+import Map from "../../components/map/Map"
 
 
 const ProductList = () => {
@@ -14,7 +15,9 @@ const ProductList = () => {
           {list.map(item => <Card key={item.id} item={item} />)}
         </div>
       </div>
-      <div className="mapContainer">map</div>
+      <div className="mapContainer">
+        <Map list={list} />
+      </div>
     </div>
   )
 }
--- client/src/pages/productlist/dummyData.js
+++ client/src/pages/productlist/dummyData.js
@@ -1,24 +1,57 @@
 export const dummyData = [
   {
-    id: 1,
-    title: "A great apartment next to the Beach!",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnQErGBXJfEiB70eQkfD-DOxHYoeMW-YdEyQ&s",
-    bedRooms: 2,
-    bathRooms: 1,
-    price: 2000000000,
-    address: "2506 Nguyễn Văn Linh, Đà Nẵng",
-    latitude: 51.5074,
-    longitude: -0.1278
+    id: 3,
+    title: "Nhà phố hiện đại trung tâm Cầu Giấy",
+    img: "https://images.unsplash.com/photo-1568605114967-8130f3a36994",
+    bedRoom: 4,
+    bathRoom: 3,
+    price: 6500000000,
+    address: "85 Duy Tân, Cầu Giấy, Hà Nội",
+    latitude: 21.0367,
+    longitude: 105.7828
   },
   {
-    id: 2,
-    title: "A great apartment next to the Beach!",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnQErGBXJfEiB70eQkfD-DOxHYoeMW-YdEyQ&s",
-    bedRooms: 2,
-    bathRooms: 1,
-    price: 2000000000,
-    address: "2506 Nguyễn Văn Linh, Đà Nẵng",
-    latitude: 51.5074,
-    longitude: -0.1278
+    id: 4,
+    title: "Chung cư cao cấp view Hồ Tây",
+    img: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
+    bedRoom: 3,
+    bathRoom: 2,
+    price: 4200000000,
+    address: "12 Lạc Long Quân, Tây Hồ, Hà Nội",
+    latitude: 21.0704,
+    longitude: 105.8188
+  },
+  {
+    id: 5,
+    title: "Biệt thự sân vườn ven sông Hàn",
+    img: "https://images.unsplash.com/photo-1600585154526-990dced4db0d",
+    bedRoom: 5,
+    bathRoom: 4,
+    price: 12500000000,
+    address: "15 Trần Hưng Đạo, Sơn Trà, Đà Nẵng",
+    latitude: 16.0678,
+    longitude: 108.2297
+  },
+  {
+    id: 6,
+    title: "Đất nền khu đô thị Hòa Xuân",
+    img: "https://images.unsplash.com/photo-1500382017468-9049fed747ef",
+    bedRoom: 0,
+    bathRoom: 0,
+    price: 2800000000,
+    address: "KĐT Hòa Xuân, Cẩm Lệ, Đà Nẵng",
+    latitude: 16.0208,
+    longitude: 108.2389
+  },
+  {
+    id: 7,
+    title: "Nhà cấp 4 gần biển Mỹ Khê",
+    img: "https://images.unsplash.com/photo-1570129477492-45c003edd2be",
+    bedRoom: 3,
+    bathRoom: 2,
+    price: 3700000000,
+    address: "120 Võ Nguyên Giáp, Ngũ Hành Sơn, Đà Nẵng",
+    latitude: 16.0544,
+    longitude: 108.2475
   }
-]
+];
