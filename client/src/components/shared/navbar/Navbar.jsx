44f1394041192f9fccbef9782aa31563d281cf42
TranDinhHieu <hieu.tran11002@gmail.com>
2026-06-20 13:57:41 +0700

    Setup Implement theme Dark & Light

--- client/src/components/shared/navbar/Navbar.jsx
+++ client/src/components/shared/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./navbar.scss"
 import { userData } from "../../../libs/dummyData"
 import { useNavigate } from "react-router-dom"
 import LanguageSwitcher from "../languageSwitcher/LanguageSwitcher"
+import ThemeSwitcher from "../themeSwitcher/ThemeSwitcher"
 
 const Navbar = () => {
   const { t } = useTranslation()
@@ -44,6 +45,7 @@ const Navbar = () => {
       </div>
 
       <div className="right">
+        <ThemeSwitcher />
         <LanguageSwitcher />
 
         {user ? (
@@ -74,7 +76,7 @@ const Navbar = () => {
                   </svg>
                 </button>
 
-                <button className="popupItem">
+                <button className="popupItem password">
                   <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <rect x="3" y="11" width="18" height="11" rx="2" ry="2"/>
                     <path d="M7 11V7a5 5 0 0 1 10 0v4"/>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.scss"
 import './i18n/index.js'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
